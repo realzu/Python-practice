c354227ec2faa7609a1e5a572829f2591276b9d6
진현주 <97022695+realzu@users.noreply.github.com>
2022-06-14 14:56:16 +0900

    나도코딩 10강-11강 완강

--- basic/practice5.py
+++ basic/practice5.py
@@ -115,6 +115,114 @@ price(5)
 
 
 # 패키지 --모듈들을 모아놓은 집합
-import travel.thailand
+import travel.thailand  #모듈or패키지만 가능 (클래스, 함수는 바로 불가)
 trip_to = travel.thailand.ThailandPackage()
 trip_to.detail()
+
+from travel.thailand import ThailandPackage #import를 통해선 가능
+trip_to = ThailandPackage()
+trip_to.detail()
+
+from travel import vietnam
+trip_to = vietnam.VietnamPackage()
+trip_to.detail()
+
+
+
+# __all__ -- __init__파일에 __all__에 사용할 패키지써줌으로써 해당 파일 사용가능
+# from random import *
+from travel import *
+# trip_to = vietnam.VietnamPackage()  
+trip_to = thailand.ThailandPackage()
+trip_to.detail()
+
+
+
+# 모듈 직접 실행
+# __name__ == "__main__" (thailand.py 확인)
+
+
+
+# 패키지, 모듈 확인
+import inspect
+import random
+print(inspect.getfile(random))  #랜덤 모듈의 파일 위치 확인
+print(inspect.getfile(thailand))
+
+
+
+# pip install   --패지키 설치 방법
+# https://pypi.org/
+from bs4 import BeautifulSoup
+soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
+print(soup.prettify())
+# pip list  --설치된 패지키 리스트
+# pip show 패지키명 --패지키정보
+# pip install --upgrade 패키지명    --가장 최신의 패지키 버전으로 업그레이드
+# pip uninstall 패키지명    --패키지 삭제
+
+
+
+# 내장함수  --이미 내장되어 import필요없이 바로 사용가능한 함수
+# input : 사용자 입력을 받는 함수
+language = input('무슨 언어를 좋아하세요?')
+print('{0}은 아주 좋은 언어입니다!'.format(language))
+
+# dir : 어떤 객체를 넘겨줬을 때 그 객체가 어떤 변수와 함수를 가지고 있는지 표시
+print(dir())
+import random # 외장 함수
+print(dir())
+import pickle
+print(dir())
+
+print(dir(random))  #랜덤모듈에서 쓸수있는것들
+lst = [1, 2, 3]
+print(dir(lst))
+
+name = 'Jin'
+print(dir(name))
+# list of python builtins
+# https://docs.python.org/3/library/functions.html
+
+
+
+# 외장함수
+# https://docs.python.org/3/py-modindex.html
+# glob : 경로 내의 폴더 / 파일 목록 조회 (윈도우 dir)
+import glob
+print(glob.glob("*.py"))    #확장자가 py인 모든 파일
+
+# os : 운영체제에서 제공하는 기본 기능
+import os
+print(os.getcwd())  #현재 디렉토리
+
+folder = "sample_dir"
+
+if os.path.exists(folder):
+    print('이미 존재하는 폴더입니다.')
+    os.rmdir(folder)
+    print(folder, '폴더를 삭제하였습니다.')
+else:
+    os.makedirs(folder) #폴더 생성
+    print(folder, '폴더를 생성하였습니다.')
+
+print(os.listdir())
+
+# time : 시간 관련 함수
+import time
+print(time.localtime())
+print(time.strftime('%Y-%m-%d %H:%M:%S'))
+
+import datetime
+# print('오늘 날짜는 ', datetime.date.today())
+
+# timedelta : 두 날짜 사이의 간격
+today = datetime.date.today()   #오늘 날짜 저장
+td = datetime.timedelta(days=100)   #100일 저장
+print('우리가 만난지 100일은', today + td)  #오늘부터 100일 후
+
+
+# Quiz
+# byme.py 추가
+import byme
+byme.sign()
